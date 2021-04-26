bd73ed7bfc8e75168e00eef8a4f42f4cb6355fb8
Shoble Thomas <mail2shobz@gmail.com>
2021-04-26 13:21:39 +0530

    updated script
    updated script with tested scenratios and fixed issues

--- script.sh
+++ script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+printf 'Enter the DB name you want to connect:'
+read -r dbname
+printf 'Enter the hostname name you want to connect:'
+read -r hostname
+printf 'Enter the username:'
+read -r username
 printf 'Enter the Query You want to save as CSV:'
 read -r  var
 echo "Execting the command"
-psql -h 127.0.0.1  -d student -U myuser -t -A -F"," -c "$var" > output.csv
+psql -h $hostname  -d $dbname -U $username -t -A -F"," -c "$var" > output.csv
